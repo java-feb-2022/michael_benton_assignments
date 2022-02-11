42a06bfc17c5a9e7cc01e0f1f7a3d410f6da6f46
Michael <92832638+Benton-Michael@users.noreply.github.com>
2022-02-11 14:52:07 -0700

    Create HelloDojo.java

---
+++ first_java_program/HelloDojo.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class HelloDojo {
+    public static void main (String[] args) {
+        System.out.println("My name is Michael");
+        System.out.println("I am 37 years old");
+        System.out.println("My hometown is Boston, MA");
+    }
+}
